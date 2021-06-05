7f47b2130598cbd406216921f5e53773f55203cd
ghdus819 <ghdus819@gmail.com>
2021-06-05 20:05:46 +0900

    210605

---
+++ 210605_BaekJoon_4355.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	long long num;
+	cin >> num;
+
+	while (num != 0) {
+		long long prime = 2;
+		long long res = num;
+
+		while (prime * prime <= num) {
+			if (num % prime == 0) {
+				res = (res / prime) * (prime - 1);
+			}
+
+			while (num % prime == 0) {
+				num /= prime;
+			}
+
+			prime++;
+		}
+
+		if (num != 1) {
+			res = (res / num) * (num - 1);
+		}
+
+		cout << res << "\n";
+		cin >> num;
+	}
+
+	return 0;
+}
