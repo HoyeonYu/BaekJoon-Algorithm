7210a7f6146d2d61949dfc24ec7d86021ba723f1
ghdus819 <ghdus819@gmail.com>
2021-06-07 14:05:51 +0900

    210606

---
+++ 210606_BaekJoon_12865.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <cmath>
+#define MAX_ITEM 100 + 1
+#define MAX_WEIGHT 100000 + 1
+#define MAX_VAL 1000 + 1
+using namespace std;
+
+int itemNum, maxWeight;
+int weightArr[MAX_WEIGHT];
+int valArr[MAX_WEIGHT];
+int knapsack[MAX_ITEM][MAX_WEIGHT] = { 0 };
+
+int main() {
+	cin >> itemNum >> maxWeight;
+
+	for (int i = 1; i <= itemNum; i++) {
+		cin >> weightArr[i] >> valArr[i];
+	}
+
+	for (int i = 1; i <= itemNum; i++) {
+		for (int j = 1; j <= maxWeight; j++) {
+			if (weightArr[i] <= j) {
+				knapsack[i][j] = max(valArr[i] + knapsack[i - 1][j - weightArr[i]],
+					knapsack[i - 1][j]);
+			}
+
+			else {
+				knapsack[i][j] = knapsack[i - 1][j];
+			}
+		}
+	}
+
+	cout << knapsack[itemNum][maxWeight];
+
+	return 0;
+}
---
+++ 210606_BaekJoon_7579.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include <cmath>
+#define MAX_SUM_COST 100 * 100 + 1
+#define MAX_ITEM 100 + 1
+using namespace std;
+
+int knapsack[MAX_ITEM][MAX_SUM_COST] = { 0 };
+int memArr[MAX_ITEM];
+int costArr[MAX_ITEM];
+
+int main() {
+	int itemNum, minMem;
+	cin >> itemNum >> minMem;
+
+	for (int i = 1; i <= itemNum; i++) {
+		cin >> memArr[i];
+	}
+
+	int sumCost = 0;
+
+	for (int i = 1; i <= itemNum; i++) {
+		cin >> costArr[i];
+		sumCost += costArr[i];
+	}
+
+	for (int i = 1; i <= itemNum; i++) {
+		for (int j = 0; j <= sumCost; j++) {
+			if (costArr[i] <= j) {
+				knapsack[i][j] = max(memArr[i] + knapsack[i - 1][j - costArr[i]], knapsack[i - 1][j]);
+			}
+
+			else {
+				knapsack[i][j] = knapsack[i - 1][j];
+			}
+		}
+	}
+	
+	for (int i = 0; i <= sumCost; i++) {
+		if (knapsack[itemNum][i] >= minMem) {
+			cout << i;
+			break;
+		}
+	}
+
+	return 0;
+}
