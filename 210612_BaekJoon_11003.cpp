ce7280d0313f48bfc4eb9ec47633313b4cc4f859
ghdus819 <ghdus819@gmail.com>
2021-06-12 00:49:52 +0900

    210612

---
+++ 210612_BaekJoon_11003.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <deque>
+#define p pair<int, int>
+using namespace std;
+
+int main() {
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+
+	int N, L;
+	cin >> N >> L;
+
+	deque<p> deq;
+
+	for (int i = 0; i < N; i++) {
+		int input;
+		cin >> input;
+
+		if (!deq.empty() && deq.front().second <= i - L) {
+			deq.pop_front();
+		}
+
+		while (!deq.empty() && deq.back().first > input) {
+			deq.pop_back();
+		}
+
+		deq.push_back({ input, i });
+
+		cout << deq.front().first << " ";
+	}
+
+	return 0;
+}
