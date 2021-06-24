8688d1e4f80dcbdbfbe66fa9dd11900f76765aa2
ghdus819 <ghdus819@gmail.com>
2021-06-25 07:05:01 +0900

    String

---
+++ 210625_BaekJoon_9935.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <deque>
+using namespace std;
+
+int main() {
+	string testStr, eraseStr;
+	cin >> testStr >> eraseStr;
+
+	deque<char> stk;
+	int testStrLen = testStr.length();
+	int eraseStrLen = eraseStr.length();
+
+	for (int i = 0; i < testStrLen; i++) {
+		stk.push_back(testStr[i]);
+
+		if (stk.size() >= eraseStrLen) {
+			string cmpStr = "";
+
+			for (int j = 0; j < eraseStrLen; j++) {
+				cmpStr = stk.back() + cmpStr;
+				stk.pop_back();
+			}
+
+			if (cmpStr != eraseStr) {
+				for (int j = 0; j < eraseStrLen; j++) {
+					stk.push_back(cmpStr[j]);
+				}
+			}
+		}
+	}
+
+	if (stk.empty()) {
+		cout << "FRULA";
+	}
+
+	else {
+		while (!stk.empty()) {
+			cout << stk.front();
+			stk.pop_front();
+		}
+	}
+
+	return 0;
+}
