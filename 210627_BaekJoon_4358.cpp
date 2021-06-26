a63466e96d5eacc04d7a8011ee71a261566d67c9
ghdus819 <ghdus819@gmail.com>
2021-06-27 06:38:13 +0900

    Map

---
+++ 210627_BaekJoon_4358.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <string>
+#include <map>
+using namespace std;
+
+int main() {
+	string treeName;
+	map<string, int> treeMap;
+	int totalCount = 0;
+
+	while (getline(cin, treeName)) {
+		if (treeMap.find(treeName) == treeMap.end()) {
+			treeMap.insert({ treeName, 1 });
+		}
+
+		else {
+			treeMap[treeName]++;
+		}
+
+		totalCount++;
+	}
+
+	for (auto it : treeMap) {
+		float rate = (int)((it.second * 100 / (float)totalCount) * 10000 + 0.5) / 10000.0;
+		cout << it.first;
+		printf(" %.4f\n", rate);
+	}
+
+	return 0;
+}
