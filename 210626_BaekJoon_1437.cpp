5f99e6de481df1f145d8ab091edd8e5aa63cf31e
ghdus819 <ghdus819@gmail.com>
2021-06-26 19:52:56 +0900

    Math

---
+++ 210626_BaekJoon_1437.cpp
@@ -0,0 +1,16 @@
+﻿#include<iostream>
+using namespace std;
+
+int main() {
+	int num;
+	cin >> num;
+
+	long long res = 1;
+
+	while (num >= 5) {
+		res = (3 * res) % 10007;
+		num -= 3;
+	}
+
+	cout << (res * num) % 10007 << endl;
+}
