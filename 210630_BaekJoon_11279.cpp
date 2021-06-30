720a0325a8f6f7b9474d757e205a5d2e9ee211d0
ghdus819 <ghdus819@gmail.com>
2021-07-01 05:22:01 +0900

    Priority Queue

---
+++ 210630_BaekJoon_11279.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <queue>
+using namespace std;
+
+int main() {
+	cin.tie(0);
+	ios::sync_with_stdio(0);
+
+	int num;
+	cin >> num;
+
+	priority_queue<int, vector<int>, less<int>> pq;
+
+	for (int i = 0; i < num; i++) {
+		int input;
+		cin >> input;
+
+		if (input == 0) {
+			if (pq.empty()) {
+				cout << 0 << "\n";
+			}
+
+			else {
+				cout << pq.top() << "\n";
+				pq.pop();
+			}
+		}
+
+		else {
+			pq.push(input);
+		}
+	}
+
+	return 0;
+}
