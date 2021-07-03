a0e3ff551f6828e3c321e15da4bb5d3e7b617ea4
ghdus819 <ghdus819@gmail.com>
2021-07-04 08:13:29 +0900

    Bit Mask

---
+++ 210704_BaekJoon_18119.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+	int strNum, cmdNum;
+	cin >> strNum >> cmdNum;
+
+	long long bitSet = ((1 << 27) - 1);
+	vector<long long> vec;
+
+	for (int i = 0; i < strNum; i++) {
+		string str;
+		cin >> str;
+
+		long long strBit = 0;
+
+		for (int j = 0; j < str.length(); j++) {
+			strBit |= (1 << (str[j] - 'a'));
+		}
+
+		vec.push_back(strBit);
+	}
+
+	for (int i = 0; i < cmdNum; i++) {
+		int cmdType;
+		char cmdChar;
+		cin >> cmdType >> cmdChar;
+
+		if (cmdType == 1) {
+			bitSet &= ~(1 << (cmdChar - 'a'));
+		}
+
+		else {
+			bitSet |= (1 << (cmdChar - 'a'));
+		}
+
+		int isSetNum = 0;
+
+		for (int j = 0; j < strNum; j++) {
+			if ((vec[j] & bitSet) == vec[j]) {
+				isSetNum++;
+			}
+		}
+
+		cout << isSetNum << "\n";
+	}
+
+	return 0;
+}
