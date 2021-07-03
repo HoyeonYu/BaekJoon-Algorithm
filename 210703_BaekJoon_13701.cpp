f673623e64dd8dad959932e88d8956dddae7e383
ghdus819 <ghdus819@gmail.com>
2021-07-03 13:03:05 +0900

    Bit Mask

---
+++ 210703_BaekJoon_13701.cpp
@@ -0,0 +1,18 @@
+﻿#include <cstdio>
+#pragma warning (disable: 4996)
+
+int num, bitArr[1 << 20] = { 0 };
+
+int main() {
+	while (scanf("%d", &num) != -1) {
+		int num1 = num / 32;
+		int num2 = 1 << (num % 32);
+
+		if (!(bitArr[num1] & num2)) {
+			bitArr[num1] += num2;
+			printf("%d ", num);
+		}
+	}
+
+	return 0;
+}
