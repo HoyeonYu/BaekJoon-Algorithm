adc2fe3d7fa46981622fe788094247f8d66c7037
ghdus819 <ghdus819@gmail.com>
2021-07-13 12:14:30 +0900

    Priority Queue

---
+++ 210713_BaekJoon_13975.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <queue>
+using namespace std;
+
+int main() {
+	int tcNum;
+	cin >> tcNum;
+
+	for (int i = 0; i < tcNum; i++) {
+		int fileNum;
+		cin >> fileNum;
+
+		priority_queue<long long, vector<long long>, greater<long long>> pq;
+
+		for (int j = 0; j < fileNum; j++) {
+			int size;
+			cin >> size;
+
+			pq.push(size);
+		}
+
+		long long sum = 0;
+
+		while (pq.size() > 1) {
+			long long first = pq.top(); pq.pop();
+			long long second = pq.top(); pq.pop();
+			pq.push(first + second);
+			sum += first + second;
+		}
+
+		cout << sum << "\n";
+	}
+
+	return 0;
+}
