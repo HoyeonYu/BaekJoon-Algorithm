51212c73daa39574783f175d043a97daf447815b
ghdus819 <ghdus819@gmail.com>
2021-07-15 19:45:32 +0900

    Math

---
+++ 210715_BaekJoon_1037.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	int num;
+	cin >> num;
+
+	vector<long long> vec;
+
+	for (int i = 0; i < num; i++) {
+		int input;
+		cin >> input;
+		vec.push_back(input);
+	}
+
+	sort(vec.begin(), vec.end());
+
+	cout << vec[0] * vec[num - 1];
+
+	return 0;
+}
