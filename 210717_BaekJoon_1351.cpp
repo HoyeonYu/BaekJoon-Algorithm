5fa9e9de4631d999a56ef0f7455dc0d0b8fbe888
ghdus819 <ghdus819@gmail.com>
2021-07-17 22:21:37 +0900

    Map

---
+++ 210717_BaekJoon_1351.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <map>
+using namespace std;
+
+long long N, P, Q;
+map<long long, long long> questMap;
+
+long long getNum(long long idx);
+
+int main() {
+	cin >> N >> P >> Q;
+	questMap.insert({ 0, 1 });
+	cout << getNum(N);
+
+	return 0;
+}
+
+long long getNum(long long idx) {
+	if (questMap.find(idx) != questMap.end()) {
+		return questMap.find(idx)->second;
+	}
+
+	questMap.insert({ idx, getNum(idx / P) + getNum(idx / Q) });
+
+	return questMap.find(idx)->second;
+}
