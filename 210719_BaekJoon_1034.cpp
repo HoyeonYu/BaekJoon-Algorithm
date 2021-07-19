5d1dfa65e84acc79472306c8d449c9e76d85db34
ghdus819 <ghdus819@gmail.com>
2021-07-19 22:37:51 +0900

    Brute Force

---
+++ 210719_BaekJoon_1034.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include <vector>
+#include <cmath>
+using namespace std;
+
+int main() {
+	int row, col, k;
+	cin >> row >> col;
+
+	vector<string> stringVec;
+	vector<int> zeroVec;
+
+	for (int i = 0; i < row; i++) {
+		string str;
+		cin >> str;
+		stringVec.push_back(str);
+		zeroVec.push_back(0);
+
+		for (int j = 0; j < col; j++) {
+			if (str[j] == '0') {
+				zeroVec[i]++;
+			}
+		}
+	}
+
+	cin >> k;
+	int maxVal = 0;
+
+	for (int i = 0; i < row; i++) {
+		int sameRow = 0;
+
+		if (zeroVec[i] <= k && (zeroVec[i] % 2 == k % 2)) {
+			for (int j = 0; j < row; j++) {
+				if (stringVec[i] == stringVec[j]) {
+					sameRow++;
+				}
+			}
+		}
+
+		maxVal = max(maxVal, sameRow);
+	}
+
+	cout << maxVal;
+
+	return 0;
+}
