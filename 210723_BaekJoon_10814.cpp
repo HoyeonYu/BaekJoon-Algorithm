149b6f0dc84ddfc794e1b8db11e3c9bbdb286d68
ghdus819 <ghdus819@gmail.com>
2021-07-23 23:56:30 +0900

    Stable Sort

---
+++ 210723_BaekJoon_10814.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+#define p pair<int, string>
+using namespace std;
+
+bool cmp(p a, p b) {
+	return a.first < b.first;
+};
+
+int main() {
+	int num;
+	cin >> num;
+	vector<p> vec;
+
+	for (int i = 0; i < num; i++) {
+		int age;
+		string name;
+		cin >> age >> name;
+		vec.push_back({ age, name });
+	}
+
+	stable_sort(vec.begin(), vec.end(), cmp);
+
+	for (int i = 0; i < num; i++) {
+		cout << vec[i].first << " " << vec[i].second << "\n";
+	}
+
+	return 0;
+}
