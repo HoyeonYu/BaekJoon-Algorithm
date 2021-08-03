04a58ccb22c46a04c09cf6742ead54cb26023927
ghdus819 <ghdus819@gmail.com>
2021-08-03 22:42:01 +0900

    Map

---
+++ 210803_BaekJoon_1620.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <map>
+#include <string>
+using namespace std;
+
+int main() {
+	cin.tie(0);
+	ios::sync_with_stdio(0);
+
+	int input, find;
+	cin >> input >> find;
+
+	map<int, string> findByIdxMap;
+	map<string, int> findByNameMap;
+
+	for (int i = 1; i <= input; i++) {
+		string str;
+		cin >> str;
+		findByIdxMap.insert({ i, str });
+		findByNameMap.insert({ str, i });
+	}
+
+	for (int i = 1; i <= find; i++) {
+		string str;
+		cin >> str;
+
+		if (isalpha(str[0])) {
+			cout << findByNameMap[str] << "\n";
+		}
+
+		else {
+			
+			cout << findByIdxMap[stoi(str)] << "\n";
+		}
+	}
+}
