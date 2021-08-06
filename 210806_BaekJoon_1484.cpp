c4e8214102ba176da55eeb4f9689f2ca5f3bd504
ghdus819 <ghdus819@gmail.com>
2021-08-06 21:16:16 +0900

    Math

---
+++ 210806_BaekJoon_1484.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	int num;
+	cin >> num;
+
+	int root = sqrt(num);
+	vector<int> vec;
+
+	for (int i = 1; i <= root; i++) {
+		if (num % i == 0) {
+			if ((num / i) == i) {
+				break;
+			}
+
+			vec.push_back(i);
+			vec.push_back(num / i);
+		}
+	}
+
+	vector<int> resVec;
+
+	for (int i = 0; i < vec.size(); i+=2) {
+		if ((vec[i] + vec[i + 1]) % 2 == 0) {
+			resVec.push_back((vec[i] + vec[i + 1]) / 2);
+		}
+	}
+
+	sort(resVec.begin(), resVec.end());
+
+	if (resVec.size() == 0) {
+		cout << -1;
+	}
+
+	for (int i = 0; i < resVec.size(); i++) {
+		cout << resVec[i] << "\n";
+	}
+
+	return 0;
+}
