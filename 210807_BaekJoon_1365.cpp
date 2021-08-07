4f12c3d1daed34923532f5d606381167a058abee
ghdus819 <ghdus819@gmail.com>
2021-08-07 18:03:41 +0900

    LIS O(NlogN)

---
+++ 210807_BaekJoon_1365.cpp
@@ -0,0 +1,56 @@
+﻿#include <iostream>
+#define MAX_SIZE 100000 + 1
+using namespace std;
+
+int binarySearch(int left, int right, int val);
+int lisArr[MAX_SIZE];
+
+int main() {
+	cin.tie(0);
+	ios::sync_with_stdio(0);
+
+	int num;
+	cin >> num;
+
+	int lisIdx = 0;
+
+	for (int i = 0; i < num; i++) {
+		int input;
+		cin >> input;
+
+		if (i == 0) {
+			lisArr[0] = input;
+		}
+
+		else {
+			if (lisArr[lisIdx] < input) {
+				lisArr[++lisIdx] = input;
+			}
+
+			else {
+				int idx = binarySearch(0, lisIdx + 1, input);
+				lisArr[idx] = input;
+			}
+		}
+	}
+
+	cout << num - (lisIdx + 1);
+
+	return 0;
+}
+
+int binarySearch(int left, int right, int val) {
+	while (left < right) {
+		int mid = (left + right) / 2;
+
+		if (lisArr[mid] < val) {
+			left = mid + 1;
+		}
+
+		else {
+			right = mid;
+		}
+	}
+
+	return right;
+}
