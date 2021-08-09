a1facbe42fdaf03793e42ed5d9c68eb711981f8c
ghdus819 <ghdus819@gmail.com>
2021-08-09 20:24:04 +0900

    LIS O(NlogN)

---
+++ 210809_BaekJoon_12014.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+#define MAX_SIZE 10000 + 1
+using namespace std;
+
+int binarySearch(int left, int right, int val);
+
+int lisArr[MAX_SIZE];
+
+int main() {
+	int tcNum;
+	cin >> tcNum;
+
+	for (int i = 0; i < tcNum; i++) {
+		int inputNum, increaseNum;
+		cin >> inputNum >> increaseNum;
+
+		int inputArr[MAX_SIZE];
+		int lisIdx = 0;
+		cin >> lisArr[lisIdx];
+
+		for (int j = 1; j < inputNum; j++) {
+			cin >> inputArr[j];
+
+			if (lisArr[lisIdx] < inputArr[j]) {
+				lisArr[++lisIdx] = inputArr[j];
+			}
+
+			else {
+				int searchIdx = binarySearch(0, lisIdx, inputArr[j]);
+				lisArr[searchIdx] = inputArr[j];
+			}
+		}
+
+		cout << "Case #" << i + 1 << "\n";
+		cout << (increaseNum <= lisIdx + 1) << "\n";
+	}
+
+	return 0;
+}
+
+int binarySearch(int left, int right, int val) {
+	while (left < right) {
+		int mid = (left + right) / 2;
+
+		if (lisArr[mid] < val) {
+			left = mid + 1;
+		}
+
+		else {
+			right = mid;
+		}
+	}
+
+	return right;
+}
---
+++ 210809_BaekJoon_1818.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+#define MAX_SIZE 200000 + 1
+using namespace std;
+
+int binarySearch(int left, int right, int val);
+
+int lisArr[MAX_SIZE];
+
+int main() {
+	cin.tie(0);
+	ios::sync_with_stdio(0);
+
+	int inputNum;
+	cin >> inputNum;
+
+	int inputArr[MAX_SIZE];
+	int lisIdx = 0;
+	cin >> lisArr[lisIdx];
+
+	for (int i = 1; i < inputNum; i++) {
+		cin >> inputArr[i];
+
+		if (lisArr[lisIdx] < inputArr[i]) {
+			lisArr[++lisIdx] = inputArr[i];
+		}
+
+		else {
+			int searchIdx = binarySearch(0, lisIdx, inputArr[i]);
+			lisArr[searchIdx] = inputArr[i];
+		}
+	}
+
+	cout << inputNum - (lisIdx + 1);
+
+	return 0;
+}
+
+int binarySearch(int left, int right, int val) {
+	while (left < right) {
+		int mid = (left + right) / 2;
+
+		if (lisArr[mid] < val) {
+			left = mid + 1;
+		}
+
+		else {
+			right = mid;
+		}
+	}
+
+	return right;
+}
