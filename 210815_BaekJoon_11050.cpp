7d2d237a72255c798dbd9ae6b2691458c834f09b
ghdus819 <ghdus819@gmail.com>
2021-08-15 23:44:38 +0900

    Math

---
+++ 210815_BaekJoon_11050.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+
+int fac(int n);
+
+int main() {
+	int n, m;
+	cin >> n >> m;
+
+	int up = fac(n);
+	int down = fac(m) * fac(n - m);
+
+	cout << up / down;
+
+	return 0;
+}
+
+int fac(int n) {
+	int res = 1;
+
+	for (int i = n; i >= 1; i--) {
+		res *= i;
+	}
+
+	return res;
+}
