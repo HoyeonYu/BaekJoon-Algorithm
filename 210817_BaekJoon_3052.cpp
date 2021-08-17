1d22447ab0347be1675b44a9d71392a16f782a86
ghdus819 <ghdus819@gmail.com>
2021-08-17 23:21:03 +0900

    Math

---
+++ 210817_BaekJoon_3052.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <map>
+using namespace std;
+
+int main() {
+	map<int, int> modMap;
+
+	for (int i = 0; i < 10; i++) {
+		int num;
+		cin >> num;
+		modMap.insert({ num % 42, 0 });
+	}
+
+	cout << modMap.size();
+
+	return 0;
+}
