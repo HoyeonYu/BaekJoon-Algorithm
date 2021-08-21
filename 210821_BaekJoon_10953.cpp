b8c299e2c563082627ee3b13c95c8c4698a65c30
ghdus819 <ghdus819@gmail.com>
2021-08-21 23:41:12 +0900

    Math

---
+++ 210821_BaekJoon_10953.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	int num;
+	cin >> num;
+
+	for (int i = 0; i < num; i++) {
+		string str;
+		cin >> str;
+		
+		cout << (str[0] - '0') + (str[2] - '0') << "\n";
+	}
+
+	return 0;
+}
