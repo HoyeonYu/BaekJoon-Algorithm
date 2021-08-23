247a22fac2d69d69c81805e64d0d701df9f713ce
ghdus819 <ghdus819@gmail.com>
2021-08-23 23:07:26 +0900

    Implementation

---
+++ 210823_BaekJoon_2908.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+	int num1, num2;
+	cin >> num1 >> num2;
+
+	int rev1 = (num1 % 10) * 100 + (num1 / 10 % 10) * 10 + (num1 / 100);
+	int rev2 = (num2 % 10) * 100 + (num2 / 10 % 10) * 10 + (num2 / 100);
+
+	cout << max(rev1, rev2);
+
+	return 0;
+}
