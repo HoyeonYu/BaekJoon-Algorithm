8fca3e4cf3def68f4200013a985bd65cd835a0c3
ghdus819 <ghdus819@gmail.com>
2021-08-28 09:49:15 +0900

    KMP

---
+++ 210828_BaekJoon_16900.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <cmath>
+#define MAX_LEN 500000 + 1
+using namespace std;
+
+int piNum;
+string str;
+int pi[MAX_LEN] = { 0 };
+
+void getPi();
+
+int main() {
+	long long num;
+	cin >> str >> num;
+
+	long long strLen = str.length();
+	getPi();
+	cout << strLen + (strLen - piNum) * (num - 1);
+
+	return 0;
+}
+
+void getPi() {
+	int j = 0;
+
+	for (int i = 1; i < str.length(); i++) {
+		while (j > 0 && str[i] != str[j]) {
+			j = pi[j - 1];
+		}
+
+		if (str[i] == str[j]) {
+			pi[i] = ++j;
+		}
+	}
+
+	piNum = pi[str.length() - 1];
+}
---
+++ 210828_BaekJoon_16916.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+#define MAX_LEN 1000000 + 1
+using namespace std;
+
+void getPi();
+bool kmp();
+
+string orgStr, findStr;
+int pi[MAX_LEN] = { 0 };
+
+int main() {
+	cin >> orgStr >> findStr;
+	getPi();
+	cout << kmp();
+
+	return 0;
+}
+
+void getPi() {
+	int j = 0;
+
+	for (int i = 1; i < findStr.length(); i++) {
+		while (j > 0 && findStr[i] != findStr[j]) {
+			j = pi[j - 1];
+		}
+
+		if (findStr[i] == findStr[j]) {
+			pi[i] = ++j;
+		}
+	}
+}
+
+bool kmp() {
+	int j = 0;
+
+	for (int i = 0; i < orgStr.length(); i++) {
+		while (j > 0 && orgStr[i] != findStr[j]) {
+			j = pi[j - 1];
+		}
+
+		if (orgStr[i] == findStr[j]) {
+			j++;
+
+			if (j == findStr.length()) {
+				return true;
+			}
+		}
+	}
+
+	return false;
+}
