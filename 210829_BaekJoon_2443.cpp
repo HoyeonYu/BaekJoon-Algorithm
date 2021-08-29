b53304b41b6600411775ba9e0a7cea141a089437
ghdus819 <ghdus819@gmail.com>
2021-08-29 23:52:03 +0900

    Implementation

---
+++ 210829_BaekJoon_2443.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	int num;
+	cin >> num;
+
+	for (int i = 0; i < num; i++) {
+		for (int j = 0; j < i; j++) {
+			cout << " ";
+		}
+
+		for (int j = i; j < 2 * num - 1 - i; j++) {
+			cout << "*";
+		}
+
+		cout << "\n";
+	}
+
+	return 0;
+}
