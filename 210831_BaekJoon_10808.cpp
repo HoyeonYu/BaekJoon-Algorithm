cfd3e2031e9e29918ed21e427a53669a2116fdb4
ghdus819 <ghdus819@gmail.com>
2021-08-31 09:15:38 +0900

    Implementation

---
+++ 210831_BaekJoon_10808.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	string str;
+	cin >> str;
+
+	int arr[26] = { 0 };
+
+	for (int i = 0; i < str.length(); i++) {
+		arr[str[i] - 'a']++;
+	}
+
+	for (int i = 0; i < 26; i++) {
+		cout << arr[i] << " ";
+	}
+
+	return 0;
+}
