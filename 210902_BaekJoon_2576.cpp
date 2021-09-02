7f5096eee3aa3cd1c7477549892a2389bd0ded46
ghdus819 <ghdus819@gmail.com>
2021-09-02 17:47:03 +0900

    Implementation

---
+++ 210902_BaekJoon_2576.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+	int minVal = 101;
+	int sum = 0;
+
+	for (int i = 0; i < 7; i++) {
+		int num;
+		cin >> num;
+
+		if (num % 2) {
+			sum += num;
+			minVal = min(minVal, num);
+		}
+	}
+
+	if (sum == 0) {
+		cout << -1;
+	}
+	
+	else {
+		cout << sum << "\n" << minVal;
+	}
+
+	return 0;
+}
