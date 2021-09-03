8f14f2da886a243ee5453457790c89c0b27b8c55
ghdus819 <ghdus819@gmail.com>
2021-09-03 23:58:14 +0900

    Implementation

---
+++ 210903_BaekJoon_2902.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	string str;
+	cin >> str;
+
+	string res = "";
+	res += str[0];
+
+	for (int i = 1; i < str.length(); i++) {
+		if (str[i - 1] == '-') {
+			res += str[i];
+		}
+	}
+
+	cout << res;
+
+	return 0;
+}
