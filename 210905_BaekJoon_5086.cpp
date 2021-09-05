f849562a06ee4cf9075252e43dc9c57ccc6da989
ghdus819 <ghdus819@gmail.com>
2021-09-05 11:35:28 +0900

    Implementation

---
+++ 210905_BaekJoon_5086.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	while (true) {
+		int num1, num2;
+		cin >> num1 >> num2;
+
+		if (num1 == 0) {
+			break;
+		}
+
+		if (num1 % num2 == 0) {
+			cout << "multiple\n";
+		}
+
+		else if (num2 % num1 == 0) {
+			cout << "factor\n";
+		}
+
+		else {
+			cout << "neither\n";
+		}
+	}
+
+	return 0;
+}
