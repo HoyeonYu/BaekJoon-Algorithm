ffb0f2159eabe0b49c5c677d0279cf5bc2da76ff
ghdus819 <ghdus819@gmail.com>
2021-09-12 23:11:35 +0900

    Sorting

---
+++ 210912_BaekJoon_15688.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+
+	int num;
+	cin >> num;
+
+	vector<int> vec;
+
+	for (int i = 0; i < num; i++) {
+		int input;
+		cin >> input;
+		vec.push_back(input);
+	}
+
+	sort(vec.begin(), vec.end());
+	
+	for (int i = 0; i < num; i++) {
+		cout << vec[i] << "\n";
+	}
+
+	return 0;
+}
