d1b1160d631ec18274afe7f11fe2fe48cc872290
ghdus819 <ghdus819@gmail.com>
2021-09-16 23:52:05 +0900

    String

---
+++ 210916_BaekJoon_2744.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	string str;
+	cin >> str;
+
+	for (int i = 0; i < str.length(); i++) {
+		if (isupper(str[i])) {
+			cout << char(str[i] - 'A' + 'a');
+		}
+
+		else {
+			cout << char(str[i] - 'a' + 'A');
+		}
+	}
+}
