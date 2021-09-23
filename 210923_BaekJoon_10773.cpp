47c084215f19db7c0183fcf970df978020422ed4
ghdus819 <ghdus819@gmail.com>
2021-09-23 22:14:54 +0900

    Stack

---
+++ 210923_BaekJoon_10773.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <stack>
+using namespace std;
+
+int main() {
+	int totNum;
+	cin >> totNum;
+
+	stack<int> stk;
+
+	for (int i = 0; i < totNum; i++) {
+		int num;
+		cin >> num;
+
+		if (num != 0) {
+			stk.push(num);
+		}
+
+		else {
+			stk.pop();
+		}
+	}
+
+	int stkSum = 0;
+
+	while (!stk.empty()) {
+		stkSum += stk.top();
+		stk.pop();
+	}
+
+	cout << stkSum;
+
+	return 0;
+}
