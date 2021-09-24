636350ff749561a8ea3f875d772b15b3867f7802
ghdus819 <ghdus819@gmail.com>
2021-09-24 23:55:44 +0900

    Math

---
+++ 210924_BaekJoon_4344.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+	int totNum;
+	cin >> totNum;
+
+	for (int i = 0; i < totNum; i++) {
+		float sum = 0;
+		int inNum;
+		cin >> inNum;
+		vector<int> vec;
+
+		for (int j = 0; j < inNum; j++) {
+			int num;
+			cin >> num;
+			vec.push_back(num);
+			sum += num;
+		}
+
+		int higher = 0;
+		double avg = sum / inNum;
+
+		for (int j = 0; j < inNum; j++) {
+			if (vec[j] > avg) {
+				higher++;
+			}
+		}
+
+		float ans = (higher / (float)inNum) * 100;
+		printf("%.3f%%\n", ans);
+	}
+		 
+	return 0;
+}
