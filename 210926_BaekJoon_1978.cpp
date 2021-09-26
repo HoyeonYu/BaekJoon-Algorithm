42f22625e126f4a3b8edc1db91001f6694db440f
ghdus819 <ghdus819@gmail.com>
2021-09-26 22:51:42 +0900

    Math

---
+++ 210926_BaekJoon_1978.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <string.h>
+#include <cmath>
+using namespace std;
+
+int main() {
+	int totNum;
+	cin >> totNum;
+
+	int sum = 0;
+
+	for (int i = 0; i < totNum; i++) {
+		int num;
+		cin >> num;
+
+		bool isPrime = true;
+
+		if (num == 1) {
+			continue;
+		}
+
+		for (int j = 2; j < num; j++) {
+			if (num % j == 0) {
+				isPrime = false;
+				break;
+			}
+		}
+
+		if (isPrime) {
+			sum++;
+		}
+	}
+
+	cout << sum;
+
+	return 0;
+}
