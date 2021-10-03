de7f4f8037d2067e99df95dcd9f6f30de1bc7119
ghdus819 <ghdus819@gmail.com>
2021-10-03 13:02:05 +0900

    Deque

---
+++ 211003_BaekJoon_18258.cpp
@@ -0,0 +1,65 @@
+﻿#include <iostream>
+#include <deque>
+using namespace std;
+
+int main() {
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+
+	int totNum;
+	cin >> totNum;
+
+	deque<int> deq;
+
+	for (int i = 0; i < totNum; i++) {
+		string str;
+		cin >> str;
+
+		if (str == "push") {
+			int num;
+			cin >> num;
+			deq.push_back(num);
+		}
+
+		else if (str == "pop") {
+			if (deq.empty()) {
+				cout << "-1\n";
+			}
+
+			else {
+				cout << deq.front() << "\n";
+				deq.pop_front();
+			}
+		}
+
+		else if (str == "size") {
+			cout << deq.size() << "\n";
+		}
+
+		else if (str == "empty") {
+			cout << deq.empty() << "\n";
+		}
+
+		else if (str == "front") {
+			if (deq.empty()) {
+				cout << "-1\n";
+			}
+
+			else {
+				cout << deq.front() << "\n";
+			}
+		}
+
+		else if (str == "back") {
+			if (deq.empty()) {
+				cout << "-1\n";
+			}
+
+			else {
+				cout << deq.back() << "\n";
+			}
+		}
+	}
+
+	return 0;
+}
