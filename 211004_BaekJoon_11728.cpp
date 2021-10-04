e88a3b3c05d87926f2f2f436eab47046579666c7
ghdus819 <ghdus819@gmail.com>
2021-10-04 22:25:40 +0900

    Two Pointer

---
+++ 211004_BaekJoon_11728.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+
+	int aNum, bNum;
+	cin >> aNum >> bNum;
+
+	vector<int> aVec, bVec;
+
+	for (int i = 0; i < aNum; i++) {
+		int num;
+		cin >> num;
+		aVec.push_back(num);
+	}
+
+	for (int i = 0; i < bNum; i++) {
+		int num;
+		cin >> num;
+		bVec.push_back(num);
+	}
+
+	int aPoint = 0, bPoint = 0;
+	vector<int> mergeVec;
+
+	for (int i = 0; i < aNum + bNum; i++) {
+		if (aPoint < aNum && bPoint < bNum) {
+			if (aVec[aPoint] < bVec[bPoint]) {
+				mergeVec.push_back(aVec[aPoint++]);
+			}
+
+			else {
+				mergeVec.push_back(bVec[bPoint++]);
+			}
+		}
+
+		else if (aPoint >= aNum) {
+			mergeVec.push_back(bVec[bPoint++]);
+		}
+
+		else {
+			mergeVec.push_back(aVec[aPoint++]);
+		}
+	}
+
+	for (auto num : mergeVec) {
+		cout << num << " ";
+	}
+
+	return 0;
+}
