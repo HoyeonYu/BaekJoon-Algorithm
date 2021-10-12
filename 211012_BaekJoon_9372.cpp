ceef57399c626aa899e80300c6a39959eac15e7e
ghdus819 <ghdus819@gmail.com>
2021-10-12 22:30:46 +0900

    Tree

---
+++ 211012_BaekJoon_9372.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	int caseNum;
+	cin >> caseNum;
+
+	for (int i = 0; i < caseNum; i++) {
+		int nodeNum, edgeNum;
+		cin >> nodeNum >> edgeNum;
+
+		for (int j = 0; j < edgeNum; j++) {
+			int node1, node2;
+			cin >> node1 >> node2;
+		}
+
+		cout << nodeNum - 1 << "\n";
+	}
+
+	return 0;
+}
