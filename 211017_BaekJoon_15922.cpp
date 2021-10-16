cc67fe68799f6c99c2f3e78a7e41937b00aec484
ghdus819 <ghdus819@gmail.com>
2021-10-17 06:01:34 +0900

    Sweeping

---
+++ 211017_BaekJoon_15922.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#define MAX_LINE 100000 + 1
+#define p pair<int, int>
+using namespace std;
+
+int main() {
+	int lineNum;
+	cin >> lineNum;
+
+	vector<p> lineVec;
+
+	for (int i = 0; i < lineNum; i++) {
+		int x, y;
+		cin >> x >> y;
+
+		lineVec.push_back({ x, y });
+	}
+
+	int startPoint = lineVec[0].first;
+	int endPoint = lineVec[0].second;
+	int lineLength = 0;
+
+	for (int i = 1; i < lineNum; i++) {
+		if (endPoint > lineVec[i].first) {
+			endPoint = max(endPoint, lineVec[i].second);
+		}
+
+		else {
+			lineLength += (endPoint - startPoint);
+
+			startPoint = lineVec[i].first;
+			endPoint = lineVec[i].second;
+		}
+	}
+
+	lineLength += (endPoint - startPoint);
+	
+	cout << lineLength;
+
+	return 0;
+}
