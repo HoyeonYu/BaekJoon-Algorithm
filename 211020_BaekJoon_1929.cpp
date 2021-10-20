c5820d5e47af2ea1a0cbfadf37daf279a86470a5
ghdus819 <ghdus819@gmail.com>
2021-10-20 18:53:46 +0900

    Math

---
+++ 211020_BaekJoon_1929.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <string.h>
+#define MAX_NUM 1000000 + 1
+using namespace std;
+
+int main() {
+	int min, max;
+	cin >> min >> max;
+
+	bool isPalin[MAX_NUM];
+	memset(isPalin, true, MAX_NUM);
+	isPalin[1] = false;
+
+	for (int i = 2; i <= max; i++) {
+		if (isPalin[i]) {
+			for (int j = i * 2; j <= max; j += i) {
+				isPalin[j] = false;
+			}
+		}
+	}
+
+	for (int i = min; i <= max; i++) {
+		if (isPalin[i]) {
+			cout << i << "\n";
+		}
+	}
+
+	return 0;
+}
