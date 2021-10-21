f043ec148f683012495f36e7e13a7512d205f0e4
ghdus819 <ghdus819@gmail.com>
2021-10-22 05:16:51 +0900

    Greedy

---
+++ 211022_BaekJoon_1439.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+	string str;
+	cin >> str;
+
+	int type1 = 0, type2 = 0;
+
+	for (int i = 1; i < str.length(); i++) {
+		if (str[i] != str[i - 1]) {
+			if (str[i] == '0') {
+				type1++;
+			}
+
+			else {
+				type2++;
+			}
+		}
+	}
+
+	if (str[str.length() - 1] == '0') {
+		type2++;
+	}
+
+	else {
+		type1++;
+	}
+
+	cout << min(type1, type2);
+
+	return 0;
+}
