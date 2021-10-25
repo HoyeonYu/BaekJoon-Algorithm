3df31c8b6cf536729de815e7b7910e3c520c8541
ghdus819 <ghdus819@gmail.com>
2021-10-26 05:57:36 +0900

    Math

---
+++ 211026_BaekJoon_2455.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+	int maxPeople = 0;
+	int curPeople = 0;
+
+	for (int i = 0; i < 4; i++) {
+		int output, input;
+		cin >> output >> input;
+
+		curPeople = curPeople + input - output;
+		maxPeople = max(maxPeople, curPeople);
+	}
+
+	cout << maxPeople;
+
+	return 0;
+}
