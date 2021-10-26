61a390c0b373f51a7995d0414c241a09ed219a66
ghdus819 <ghdus819@gmail.com>
2021-10-27 04:08:50 +0900

    Greedy

---
+++ 211027_BaekJoon_10162.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	int num;
+	cin >> num;
+
+	int a = num / 300;
+	num -= a * 300;
+
+	int b = num / 60;
+	num -= b * 60;
+
+	int c = num / 10;
+	num -= c * 10;
+
+	if (num != 0) {
+		cout << -1;
+	}
+
+	else {
+		cout << a << " " << b << " " << c;
+	}
+
+	return 0;
+}
