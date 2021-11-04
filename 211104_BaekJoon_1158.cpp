23fa0cd9be750ab57b77130e1933d73534c7dbf9
ghdus819 <ghdus819@gmail.com>
2021-11-04 19:28:48 +0900

    Queue

---
+++ 211104_BaekJoon_1158.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <queue>
+using namespace std;
+
+int main() {
+	int n, k;
+	cin >> n >> k;
+
+	queue<int> que;
+
+	for (int i = 1; i <= n; i++) {
+		que.push(i);
+	}
+
+	cout << "<";
+
+	while (!que.empty()) {
+		for (int i = 0; i < k - 1; i++) {
+			que.push(que.front());
+			que.pop();
+		}
+
+		cout << que.front();
+
+		if (que.size() > 1) {
+			cout << ", ";
+		}
+
+		que.pop();
+	}
+
+	cout << ">";
+
+	return 0;
+}
