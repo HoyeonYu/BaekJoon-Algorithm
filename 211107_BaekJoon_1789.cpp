99780c967c199c9eadad41bd587acd2e8f9c31a2
ghdus819 <ghdus819@gmail.com>
2021-11-07 18:04:13 +0900

    Greedy

---
+++ 211107_BaekJoon_1789.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	long long sum;
+	cin >> sum;
+
+	long long res = 1, calc = 0;
+
+	while (true) {
+		calc = (res * (res + 1)) / 2;
+
+		if (calc > sum) {
+			cout << res - 1;
+			break;
+		}
+
+		res++;
+	}
+
+	return 0;
+}
---
+++ 211107_BaekJoon_9576.cpp
@@ -0,0 +1,58 @@
+﻿#include <iostream>
+#include <algorithm>
+#define p pair<int, int>
+#define MAX_NUM 1000 + 1
+using namespace std;
+
+bool cmp(p child, p parent) {
+    if (child.second < parent.second) {
+        return true;
+    }
+
+    else {
+        if (child.second == parent.second) {
+            if (child.first < parent.first) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
+
+int main() {
+    int testCase;
+    cin >> testCase;
+
+    for (int tc = 0; tc < testCase; tc++) {
+        int n, m;
+        cin >> n >> m;
+
+        pair<int, int> student[MAX_NUM];
+        bool chosen[MAX_NUM] = { false };
+
+        for (int i = 0; i < m; i++) {
+            int a, b;
+            cin >> a >> b;
+            student[i] = { a, b };
+        }
+
+        sort(student, student + m, cmp);
+        int max_cnt = 0;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = student[i].first; j <= student[i].second; j++) {
+                if (!chosen[j]) {
+                    chosen[j] = true;
+                    max_cnt++;
+
+                    break;
+                }
+            }
+        }
+
+        cout << max_cnt << "\n";
+    }
+
+    return 0;
+}
