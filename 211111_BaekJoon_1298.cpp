04d3feae6f98cc40d2fc85865451f42f784abba8
ghdus819 <ghdus819@gmail.com>
2021-11-11 17:37:14 +0900

    Bipartite Matching

---
+++ 211111_BaekJoon_1298.cpp
@@ -0,0 +1,57 @@
+﻿#include <iostream>
+#include <vector>
+#define MAX_COW 200 + 1
+using namespace std;
+
+int cowNum, houseNum;
+vector<int> cowHouse[MAX_COW];
+bool isVisited[MAX_COW] = { false };
+int decidedHouse[MAX_COW];
+
+bool dfs(int x);
+
+int main() {
+	cin >> cowNum >> houseNum;
+
+	for (int i = 0; i < houseNum; i++) {
+		int personIdx, noteBookIdx;
+		cin >> personIdx >> noteBookIdx;
+
+		cowHouse[personIdx].push_back(noteBookIdx);
+	}
+
+	int res = 0;
+	fill_n(decidedHouse, MAX_COW, -1);
+
+	for (int i = 1; i <= cowNum; i++) {
+		fill_n(isVisited, MAX_COW, false);
+
+		if (dfs(i)) {
+			res++;
+		}
+	}
+
+	cout << res;
+
+	return 0;
+}
+
+bool dfs(int x) {
+	for (int i = 0; i < cowHouse[x].size(); i++) {
+		int houseIdx = cowHouse[x][i];
+
+		if (isVisited[houseIdx]) {
+			continue;
+		}
+
+		isVisited[houseIdx] = true;
+
+		if (decidedHouse[houseIdx] == -1 || dfs(decidedHouse[houseIdx])) {
+			decidedHouse[houseIdx] = x;
+
+			return true;
+		}
+	}
+
+	return false;
+}
---
+++ 211111_BaekJoon_2188.cpp
@@ -0,0 +1,62 @@
+﻿#include <iostream>
+#include <vector>
+#define MAX_COW 200 + 1
+using namespace std;
+
+int cowNum, houseNum;
+vector<int> cowHouse[MAX_COW];
+bool isVisited[MAX_COW] = { false };
+int decidedHouse[MAX_COW];
+
+bool dfs(int x);
+
+int main() {
+	cin >> cowNum >> houseNum;
+
+	for (int i = 0; i < cowNum; i++) {
+		int likeHouseNum;
+		cin >> likeHouseNum;
+
+		for (int j = 0; j < likeHouseNum; j++) {
+			int houseIdx;
+			cin >> houseIdx;
+
+			cowHouse[i].push_back(houseIdx);
+		}
+	}
+
+	int res = 0;
+	fill_n(decidedHouse, MAX_COW, -1);
+
+	for (int i = 0; i < cowNum; i++) {
+		fill_n(isVisited, MAX_COW, false);
+
+		if (dfs(i)) {
+			res++;
+		}
+	}
+
+	cout << res;
+
+	return 0;
+}
+
+bool dfs(int x) {
+	for (int i = 0; i < cowHouse[x].size(); i++) {
+		int houseIdx = cowHouse[x][i];
+
+		if (isVisited[houseIdx]) {
+			continue;
+		}
+
+		isVisited[houseIdx] = true;
+
+		if (decidedHouse[houseIdx] == -1 || dfs(decidedHouse[houseIdx])) {
+			decidedHouse[houseIdx] = x;
+			
+			return true;
+		}
+	}
+	
+	return false;
+}
