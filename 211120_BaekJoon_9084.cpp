f2a370e55fb113cb544267f6633769803dff460a
ghdus819 <ghdus819@gmail.com>
2021-11-20 23:55:25 +0900

    DP

---
+++ 211120_BaekJoon_9084.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#define MAX_TYPE 20 + 1
+#define MAX_AMOUNT 10000 + 1
+using namespace std;
+
+int main() {
+    int testCase;
+    cin >> testCase;
+
+    for (int i = 0; i < testCase; i++) {
+        int coinType, targetAmount;
+        cin >> coinType;
+
+        int coin[MAX_TYPE] = { 0 };
+
+        for (int j = 1; j <= coinType; j++) {
+            cin >> coin[j];
+        }
+
+        cin >> targetAmount;
+
+        int coinDP[MAX_AMOUNT] = { 0 };
+        coinDP[0] = 1;
+
+        for (int j = 1; j <= coinType; j++) {
+            for (int k = coin[j]; k <= targetAmount; k++) {
+                coinDP[k] += coinDP[k - coin[j]];
+            }
+        }
+
+        cout << coinDP[targetAmount] << "\n";
+    }
+
+    return 0;
+}
