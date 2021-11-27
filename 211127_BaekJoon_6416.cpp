22af048a6566934478b69527aa9cc53881db5dca
ghdus819 <ghdus819@gmail.com>
2021-11-27 23:58:45 +0900

    Tree

---
+++ 211127_BaekJoon_6416.cpp
@@ -0,0 +1,58 @@
+﻿#include <iostream>
+#include <map>
+using namespace std;
+
+map<int, int> M, indegree;
+
+int main() {
+    int testCases = 0;
+
+    while (true) {
+        bool ans = true;
+        int inVtx, outVtx, edgeCnt = 0, nodeCnt = 0;
+        M.clear();
+        indegree.clear();
+
+        while (true) {
+            cin >> inVtx >> outVtx;
+
+            if (inVtx == 0 && outVtx == 0) {
+                break;
+            }
+
+            if (inVtx == -1 && outVtx == -1) {
+                return 0;
+            }
+
+            edgeCnt++;
+
+            if (M.find(inVtx) == M.end()) {
+                M.insert({ inVtx, nodeCnt++ });
+            }
+
+            if (M.find(outVtx) == M.end()) {
+                M.insert({ outVtx, nodeCnt++ });
+            }
+
+            if (indegree.find(outVtx) == indegree.end())
+                indegree.insert({ outVtx, 0 });
+
+            else ans = false;
+        }
+
+        if (edgeCnt > 0 && M.size() != edgeCnt + 1) {
+            ans = false;
+        }
+
+        if (ans) {
+            printf("Case %d is a tree.\n", ++testCases);
+        }
+
+        else {
+            printf("Case %d is not a tree.\n", ++testCases);
+        }
+    }
+
+    return 0;
+}
+
