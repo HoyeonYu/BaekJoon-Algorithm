f1457e790f2e57a92b144cf2889bcde8c07a8a42
ghdus819 <ghdus819@gmail.com>
2021-11-28 23:59:41 +0900

    Tree

---
+++ 211128_BaekJoon_9489.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	while (true) {
+		int nodeNum, findNum, a[1111] = { -1 }, p[1111] = { -1 };
+		cin >> nodeNum >> findNum;
+
+		if (!nodeNum && !findNum) {
+			break;
+		}
+
+		int cnt = -1, idx = 0;
+		for (int i = 1; i <= nodeNum; i++) {
+			scanf("%d", &a[i]);
+
+			if (a[i] == findNum) idx = i;
+			if (a[i] != a[i - 1] + 1) cnt++;
+
+			p[i] = cnt;
+		}
+
+		int ans = 0;
+		for (int i = 1; i <= nodeNum; i++)
+			if (p[i] != p[idx] && p[p[i]] == p[p[idx]]) ans++;
+
+		printf("%d\n", ans);
+	}
+
+	return 0;
+}
