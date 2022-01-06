9801e06e33778b4edfb1f007b8ecf836c343cc26
ghdus819 <ghdus819@gmail.com>
2022-01-06 23:54:13 +0900

    Math

---
+++ 220106_BaekJoon_1024.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#define MAX_LEN 100
+using namespace std;
+
+int main() {
+	int N, L;
+	cin >> N >> L;
+
+	for (int i = L; i <= MAX_LEN; i++) {
+		int Lx = N - (i * (i + 1) / 2);
+		
+		if (Lx % i == 0) {
+			int x = Lx / i + 1;
+
+			if (x >= 0) {
+				for (int j = 0; j < i; j++) {
+					cout << x + j << " ";
+				}
+
+				return 0;
+			}
+		}
+	}
+
+	cout << -1;
+
+	return 0;
+}
