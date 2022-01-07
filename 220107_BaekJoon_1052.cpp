aeff169fb8d0ad41a3d810b2d4cdd51a6c7643b2
ghdus819 <ghdus819@gmail.com>
2022-01-07 23:53:24 +0900

    Bitmasking

---
+++ 220107_BaekJoon_1052.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	int N, K;
+	cin >> N >> K;
+
+	int increasedN = N;
+
+	while (true) {
+		int cnt = 0, tmpN = increasedN;
+
+		while (tmpN > 0) {
+			cnt += (tmpN % 2);
+			tmpN /= 2;
+		}
+
+		if (cnt <= K) {
+			cout << (increasedN - N);
+			break;
+		}
+
+		increasedN++;
+	}
+
+	return 0;
+}
